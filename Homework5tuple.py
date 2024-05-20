12236b7e8ef483770e6e1cd365986b9fd0f6c0a4
Tomas26071988 <artem_1988_artem1988@icloud.com>
2024-05-20 23:42:51 +0300

    Add files via upload

---
+++ Homework5tuple.py
@@ -0,0 +1,7 @@
+immutable_var = 1, 2, 'String', True
+print(immutable_var)
+# immutable_var[0] = 100
+mutable_list = (1, 2, 0) + (1, 2)
+print(mutable_list)
+mutable_list[0][0] = 2
+print(mutable_list)
